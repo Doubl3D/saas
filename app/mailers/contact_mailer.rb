9c87eb49496fa02f3a663a33d56a060bf2db4cb6
Daniel Dugan <ddugan11@gmail.com>
2015-03-04 07:16:26 +0000

    added mailer view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'ddugan11@gmail.com'  
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Dev Match Contact Form Submission')
+    
+  end  
+  
+end  
