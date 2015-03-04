bda07bb44c6866688e07349aa20553290d6b4825
Daniel Dugan <ddugan11@gmail.com>
2015-03-04 04:38:24 +0000

    added feedback functionality for successful/unsuccessful form submission

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,20 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
-  
-  def create
-  end  
+    def new
+        @contact = Contact.new
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+          flash[:success] = "Thanks for the feedback!"
+          redirect_to new_contact_path
+        else  
+          flash[:danger] = "Oh no! Something broke!"
+          redirect_to new_contact_path
+        end  
+    end  
+    private
+        def contact_params
+          params.require(:contact).permit(:name, :email, :comments)
+        end  
 end
